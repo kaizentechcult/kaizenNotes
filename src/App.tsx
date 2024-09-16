8d8aa080cdfdd097507083c04ebc92984619e653
Sooraj002 <soorajnambiar8@gmail.com>
2024-09-16 23:26:52 +0530

    added login and register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import type { Component } from "solid-js";
 import { Router, Route } from "@solidjs/router";
 
 import Home from "./pages/Home";
+import { Login } from "./pages/Login";
 const App: Component = () => {
   return (
     <Router>
       <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
     </Router>
   );
 };
