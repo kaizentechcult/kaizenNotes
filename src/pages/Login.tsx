524b1fb24c8dd91b928ce31af58d4419a01df6e5
Sooraj002 <soorajnambiar8@gmail.com>
2024-09-17 09:26:44 +0530

    data going to backend

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,52 +1,43 @@
 import { createSignal } from "solid-js";
 import { Component } from "solid-js";
-import axios from "axios";
+
 export const Login: Component = () => {
   const [email, setEmail] = createSignal<string>("");
   const [password, setPassword] = createSignal<string>("");
 
-  const handleSubmit = (e: SubmitEvent) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    console.log({ name: email(), password: password() });
 
-    // Login
-    let data = JSON.stringify({
-      password: "iloveboobs",
-      email: "krish@gmail.com",
-    });
+    const data = { email: email(), password: password() };
 
-    let config = {
-      method: "post",
-      mode: "no-cors",
-      maxBodyLength: Infinity,
-      url: "http://localhost:8080/login",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: data,
-    };
-
-    axios
-      .request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       });
+
+      // if (!response.ok) {
+      //   throw new Error("Network response was not ok");
+      // }
+      const result = await response.json();
+      console.log("Data sent successfully:", result);
+    } catch (error) {
+      console.error("Error sending data:", error);
+    }
   };
 
-  console.log(import.meta.env.VITE_MONGO_URI);
-
   return (
-    <div class="flex h-screen items-center justify-center bg-gray-100">
+    <div class="flex h-screen items-center justify-center bg-black ">
       <form
         onSubmit={handleSubmit}
-        class="w-full max-w-sm bg-white rounded-lg shadow-md p-6"
+        class="w-full max-w-sm bg-[#414141] rounded-lg shadow-md p-6"
       >
-        <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+        <h2 class="text-2xl font-bold mb-6 text-center text-white">Login</h2>
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+          <label class="block text-white text-sm font-bold mb-2" for="name">
             Name
           </label>
           <input
@@ -54,15 +45,12 @@ export const Login: Component = () => {
             type="text"
             value={email()}
             onChange={(e) => setEmail(e.currentTarget.value)}
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Enter your name"
           />
         </div>
         <div class="mb-6">
-          <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
-          >
+          <label class="block text-white text-sm font-bold mb-2" for="password">
             Password
           </label>
           <input
@@ -70,7 +58,7 @@ export const Login: Component = () => {
             type="password"
             value={password()}
             onChange={(e) => setPassword(e.currentTarget.value)}
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Enter your password"
           />
         </div>
