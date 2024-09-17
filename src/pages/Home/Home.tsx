270a13e8564b4308d3c99069328b01063c8fe7c3
krishkh <krishkhanna00@gmail.com>
2024-09-17 15:55:42 +0530

    auth ho gy a

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
