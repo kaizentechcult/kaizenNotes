f774b7cc2f7e439e5b0c3c0777bb03aefc909d46
kaizen software development club <kaizentechcult@gmail.com>
2024-09-18 20:49:22 +0530

    changes

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,8 +4,8 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)} type="button">
-      Clicks: {count()}
+    <button class="increment" onClick={() => setCount(count() + 1)}>
+      Clicks: {count}
     </button>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,9 @@
-import { Title } from "@solidjs/meta";
 import Counter from "~/components/Counter";
+import "./index.css";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
       <p>
@@ -12,7 +11,7 @@ export default function Home() {
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to learn how to build Solid apps.
       </p>
     </main>
   );
