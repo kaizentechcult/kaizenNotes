6985fd54fbc71fbe63047cd8baf8d1d4df39b6e5
kaizen software development club <kaizentechcult@gmail.com>
2024-10-14 01:38:03 +0530

    removed forget btn

--- src/components/authComponents/ForgetPass.tsx
+++ src/components/authComponents/ForgetPass.tsx
@@ -1,10 +1,11 @@
 const ForgetPass = () => {
   return (
-    <div class="flex justify-center mb-6">
-      <a href="#" class="text-linkBlue hover:underline">
-        Forgot Password?
-      </a>
-    </div>
+    // <div class="flex justify-center mb-6">
+    //   <a href="#" class="text-linkBlue hover:underline">
+    //     Forgot Password?
+    //   </a>
+    // </div>
+    <></>
   );
 };
 
