7ab57c05e89199ec6b40236a5000e19b0133d021
kaizen software development club <kaizentechcult@gmail.com>
2024-10-14 01:35:50 +0530

    fixes

--- src/utils/navData.ts
+++ src/utils/navData.ts
@@ -1,2 +1,3 @@
-export const navLinks = ["Home", "Quiz", "Profile", "About Us"];
+// export const navLinks = ["Home", "Quiz", "Profile", "About Us"];
+export const navLinks = [""];
 export const navBtns = ["Theme", "Logout"];
