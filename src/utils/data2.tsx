67ece8d9cbf2ddf07b59e4fdbe9211fc9f863dd8
sooraj002 <soorajnambiar8@gmail.com>
2024-11-05 15:47:27 +0530

    fixed maths data

--- src/utils/data2.tsx
+++ src/utils/data2.tsx
@@ -13,27 +13,27 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Algorithm Time space complexity ADT",
-            link: "https://drive.google.com/file/d/1Mdip8lcqdtfoxEmVStgpntsOnF407ckB/preview",
+            link: "https://drive.google.com/file/d/1Mdip8lcqdtfoxEmVStgpntsOnF407ckB/view",
           },
           {
             name: "Array Address Calculation",
-            link: "https://drive.google.com/file/d/1Bb2fP28g5hK9f1o7sZQrf_SAEJlO3pB4/preview",
+            link: "https://drive.google.com/file/d/1Bb2fP28g5hK9f1o7sZQrf_SAEJlO3pB4/view",
           },
           {
             name: "Linked list and vs array",
-            link: "https://drive.google.com/file/d/1j1lEFd2dIjnVhs5ysMlRTT07GkDjGaHB/preview",
+            link: "https://drive.google.com/file/d/1j1lEFd2dIjnVhs5ysMlRTT07GkDjGaHB/view",
           },
           {
             name: "Unit  1   Introduction_Array_Asymptotic notations",
-            link: "https://drive.google.com/file/d/1-CGz51Jgp3-07iwEjVHFbtqcjI9FnnV3/preview",
+            link: "https://drive.google.com/file/d/1-CGz51Jgp3-07iwEjVHFbtqcjI9FnnV3/view",
           },
           {
             name: "UNIT 1  Polynomials  its Linked List Representation",
-            link: "https://drive.google.com/file/d/1AuFQobfgqP20oX-kpXPbP7xDD9ySwNzh/preview",
+            link: "https://drive.google.com/file/d/1AuFQobfgqP20oX-kpXPbP7xDD9ySwNzh/view",
           },
           {
             name: "UNIT 1 -Linked List",
-            link: "https://drive.google.com/file/d/1D540iWIF4dt-HFdoKRPGNBzKdpLCNBJd/preview",
+            link: "https://drive.google.com/file/d/1D540iWIF4dt-HFdoKRPGNBzKdpLCNBJd/view",
           },
         ],
       },
@@ -42,12 +42,12 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Dijkstra Algorithm-Preeti Garg",
-            link: "https://docs.google.com/presentation/d/1u7KQe9oGgYvnMBCMcSdfSkgZj3Wdmxz-/edit&ouid=101780945977141676347&rtpof=true&sd=true",
+            link: "https://docs.google.com/presentation/d/1u7KQe9oGgYvnMBCMcSdfSkgZj3Wdmxz-/view",
           },
 
           {
             name: "Floyd Warshall Algorithm-Preeti Garg",
-            link: "https://docs.google.com/presentation/d/1rNXZ-9MPQNVQAdiEprRl5T2wt6K93uYB/edit&ouid=101780945977141676347&rtpof=true&sd=true",
+            link: "https://docs.google.com/presentation/d/1rNXZ-9MPQNVQAdiEprRl5T2wt6K93uYB/view",
           },
         ],
       },
@@ -56,37 +56,37 @@ let folders: Folder[] = [
         folders: [
           {
             name: "bubble sort_129",
-            link: "https://drive.google.com/file/d/1qg6Ie-sYMw-JkYAaoikACfFRcTpH7QiP/preview",
+            link: "https://drive.google.com/file/d/1qg6Ie-sYMw-JkYAaoikACfFRcTpH7QiP/view",
           },
 
           {
             name: "Hashing",
-            link: "https://drive.google.com/file/d/1HYgjUVI2r6JXnD6ZjTb7SbmQ_0n6GSiW/preview",
+            link: "https://drive.google.com/file/d/1HYgjUVI2r6JXnD6ZjTb7SbmQ_0n6GSiW/view",
           },
 
           {
             name: "HeapSort",
-            link: "https://drive.google.com/file/d/1fJCx39gL2ff8PxIXHnDrNUXMvRN2rH2z/preview",
+            link: "https://drive.google.com/file/d/1fJCx39gL2ff8PxIXHnDrNUXMvRN2rH2z/view",
           },
 
           {
             name: "Merge Sort",
-            link: "https://drive.google.com/file/d/18PIgJNRVO-23XWSpSJD6-8SBd1LRnUh9/preview",
+            link: "https://drive.google.com/file/d/18PIgJNRVO-23XWSpSJD6-8SBd1LRnUh9/view",
           },
 
           {
             name: "MergeSort",
-            link: "https://drive.google.com/file/d/15gLfXRJSlTwYTD5s5be47DI18OizhvIY/preview",
+            link: "https://drive.google.com/file/d/15gLfXRJSlTwYTD5s5be47DI18OizhvIY/view",
           },
 
           {
             name: "Quick sort-Preeti Garg",
-            link: "https://docs.google.com/presentation/d/1rmzO2Q2sy2um7kgQD63ZbzP-0idGUU9J/edit&ouid=101780945977141676347&rtpof=true&sd=true",
+            link: "https://docs.google.com/presentation/d/1rmzO2Q2sy2um7kgQD63ZbzP-0idGUU9J/view",
           },
 
           {
             name: "SELECTION SORT (1)",
-            link: "https://docs.google.com/presentation/d/1XB-ssv-DKadj-yXky3CGKKg5dm2bxga3/edit&ouid=101780945977141676347&rtpof=true&sd=true",
+            link: "https://docs.google.com/presentation/d/1XB-ssv-DKadj-yXky3CGKKg5dm2bxga3/view",
           },
         ],
       },
@@ -95,32 +95,32 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Implementation of Stack",
-            link: "https://drive.google.com/file/d/1EUAZ8Q14IHUdtrQ0uFmDa9tvNxqOHeHF/preview",
+            link: "https://drive.google.com/file/d/1EUAZ8Q14IHUdtrQ0uFmDa9tvNxqOHeHF/view",
           },
 
           {
             name: "Infix to Postfix and Infix to Prefix Conversion",
-            link: "https://drive.google.com/file/d/1ToLgufFoX2Rot_rGbPv-Hl98gxxMpuNA/preview",
+            link: "https://drive.google.com/file/d/1ToLgufFoX2Rot_rGbPv-Hl98gxxMpuNA/view",
           },
 
           {
             name: "Recursion Examples",
-            link: "https://drive.google.com/file/d/1emEb8hKKi-jcq_6ArefKJSzOttHqQH-A/preview",
+            link: "https://drive.google.com/file/d/1emEb8hKKi-jcq_6ArefKJSzOttHqQH-A/view",
           },
 
           {
             name: "Recursion",
-            link: "https://drive.google.com/file/d/1DL4H74s9nPFs1EUB8oN_X1ah2AGXSRFZ/preview",
+            link: "https://drive.google.com/file/d/1DL4H74s9nPFs1EUB8oN_X1ah2AGXSRFZ/view",
           },
 
           {
             name: "Stack",
-            link: "https://drive.google.com/file/d/1UJBH9sAk65tD5w0lTuWFQm7IPxuA4xHP/preview",
+            link: "https://drive.google.com/file/d/1UJBH9sAk65tD5w0lTuWFQm7IPxuA4xHP/view",
           },
 
           {
             name: "Tower of Hanoi",
-            link: "https://drive.google.com/file/d/1FvykJLpXotmMIMpTWebkM0gZAr_giynm/preview",
+            link: "https://drive.google.com/file/d/1FvykJLpXotmMIMpTWebkM0gZAr_giynm/view",
           },
         ],
       },
@@ -129,11 +129,11 @@ let folders: Folder[] = [
         folders: [
           {
             name: "B-Trees-Preeti Garg",
-            link: "https://docs.google.com/presentation/d/1eB8kktZe2mZrKN3bvlZPUsrvmNLuopJj/edit&ouid=101780945977141676347&rtpof=true&sd=true",
+            link: "https://docs.google.com/presentation/d/1eB8kktZe2mZrKN3bvlZPUsrvmNLuopJj/view",
           },
           {
             name: "Spanning Tree",
-            link: "https://docs.google.com/presentation/d/1ZVEyzBXg7XIKSejGbhav7ffrqO1kMGr-/edit&ouid=101780945977141676347&rtpof=true&sd=true",
+            link: "https://docs.google.com/presentation/d/1ZVEyzBXg7XIKSejGbhav7ffrqO1kMGr-/view",
           },
         ],
       },
@@ -147,7 +147,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "UNIT-1 notes ",
-            link: "https://drive.google.com/open?id=1a3uMsXryfR2FBuhGd_7W7qjl7wzJyGo1&usp=drive_copy",
+            link: "https://drive.google.com/file/d/1a3uMsXryfR2FBuhGd_7W7qjl7wzJyGo1/view",
           },
         ],
       },
@@ -156,7 +156,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Unit -2",
-            link: "https://drive.google.com/open?id=1GliAl1o16txX3QLAEGPEndYKVIIld8SU&usp=drive_copy",
+            link: "https://drive.google.com/file/d/1UZbjjwjwXg5lUrve09RU-T0_kzjVjD6V/view",
           },
         ],
       },
@@ -165,7 +165,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "KAS-302, Unit-3, notes maths",
-            link: "https://drive.google.com/open?id=1a3uMsXryfR2FBuhGd_7W7qjl7wzJyGo1&usp=drive_copy",
+            link: "https://drive.google.com/file/d/1A4lMg6EfoUfermQYo-y26nfGvLJFnE-P/view?usp=drive_link",
           },
         ],
       },
@@ -174,7 +174,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Unit -4",
-            link: "https://drive.google.com/open?id=1LQtkD9ZbiU7rZCJVS_HoYnVoZXqOdqjY&usp=drive_copy",
+            link: "https://drive.google.com/file/d/1Qu7oznxOZPRMbECwvMAJGfNua0JLstvf/view",
           },
         ],
       },
@@ -183,7 +183,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "KAS-302, Maths, Unit-5 notes",
-            link: "https://drive.google.com/open?id=1KcEhDVcFxq2cDQOQoZ7sz62wKnmZBrf0&usp=drive_copy",
+            link: "https://drive.google.com/file/d/1KcEhDVcFxq2cDQOQoZ7sz62wKnmZBrf0/view?usp=drive_link",
           },
         ],
       },
@@ -192,7 +192,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "mathematics 4_quantum",
-            link: "https://drive.google.com/open?id=15cSCFN1KtPt7OP2UYuvhdq1Ez2MgswGq&usp=drive_copy",
+            link: "https://drive.google.com/file/d/15cSCFN1KtPt7OP2UYuvhdq1Ez2MgswGq/view",
           },
         ],
       },
@@ -206,7 +206,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: " Introduction",
-            link: "https://drive.google.com/file/d/1dRMAPGEnLdRTc7y-IfYzh1jh_TtdWKM8/preview",
+            link: "https://drive.google.com/file/d/1dRMAPGEnLdRTc7y-IfYzh1jh_TtdWKM8/view",
           },
         ],
       },
@@ -215,7 +215,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Arithmetic and logic unit: ",
-            link: "https://drive.google.com/file/d/18pZZqtKftEMMwbrXaHGoEfyP-28-SFfC/preview",
+            link: "https://drive.google.com/file/d/18pZZqtKftEMMwbrXaHGoEfyP-28-SFfC/view",
           },
         ],
       },
@@ -224,7 +224,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Control Unit:",
-            link: "https://drive.google.com/file/d/14skboggHYS_Z63DTehbMPkHc2amvWE-_/preview",
+            link: "https://drive.google.com/file/d/14skboggHYS_Z63DTehbMPkHc2amvWE-_/view",
           },
         ],
       },
@@ -233,7 +233,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Memory:",
-            link: "https://drive.google.com/file/d/1D06mNjD9XPDD7Xh0Qqger_HAEUCHM61E/preview",
+            link: "https://drive.google.com/file/d/1D06mNjD9XPDD7Xh0Qqger_HAEUCHM61E/view",
           },
         ],
       },
@@ -242,7 +242,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Input / Output:",
-            link: "https://drive.google.com/file/d/1id6tnFT-EJAiOhnv_OtNlKbcNWNXw7-S/preview",
+            link: "https://drive.google.com/file/d/1id6tnFT-EJAiOhnv_OtNlKbcNWNXw7-S/view",
           },
         ],
       },
@@ -256,7 +256,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Introduction",
-            link: "https://drive.google.com/file/d/1EtI8Urb5AUWmpj8lu75qVBEq94Ik8Mt7/preview",
+            link: "https://drive.google.com/file/d/1EtI8Urb5AUWmpj8lu75qVBEq94Ik8Mt7/view",
           },
         ],
       },
@@ -265,7 +265,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Concurrent Processes:",
-            link: "https://drive.google.com/file/d/1rfmdpJ6gQn_RLdoiLdnPqB9rzDLrIMyh/preview",
+            link: "https://drive.google.com/file/d/1rfmdpJ6gQn_RLdoiLdnPqB9rzDLrIMyh/view",
           },
         ],
       },
@@ -274,7 +274,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "CPU Scheduling:",
-            link: "https://drive.google.com/file/d/102oHHJ_C8TiEfgZOAJC84pWiGoUNXkV5/preview",
+            link: "https://drive.google.com/file/d/102oHHJ_C8TiEfgZOAJC84pWiGoUNXkV5/view",
           },
         ],
       },
@@ -283,7 +283,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Memory Management:",
-            link: "https://drive.google.com/file/d/1I-X9owZGbI8kopc4u6UfaXcA_VkptqbP/preview",
+            link: "https://drive.google.com/file/d/1I-X9owZGbI8kopc4u6UfaXcA_VkptqbP/view",
           },
         ],
       },
@@ -292,7 +292,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "I/O Management and Disk Scheduling: ",
-            link: "https://drive.google.com/file/d/1Ia5BXFjlCg3ZT9uFxtC9J9_mGLOWDT2N/preview",
+            link: "https://drive.google.com/file/d/1Ia5BXFjlCg3ZT9uFxtC9J9_mGLOWDT2N/view",
           },
         ],
       },
@@ -302,7 +302,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "OS Quantum ",
-            link: "https://drive.google.com/file/d/1sind7fsCpn20F7OKMbGyye85xCQ9Eb5u/preview",
+            link: "https://drive.google.com/file/d/1sind7fsCpn20F7OKMbGyye85xCQ9Eb5u/view",
           },
         ],
       },
@@ -313,27 +313,27 @@ let folders: Folder[] = [
     folders: [
       {
         name: "Copy of python quantum .pdf",
-        link: "https://drive.google.com/file/d/1y0b-XdJvnQgk87H_DIFvwI9NwqxLEkNA/preview",
+        link: "https://drive.google.com/file/d/1y0b-XdJvnQgk87H_DIFvwI9NwqxLEkNA/view",
       },
       {
         name: "Python unit 1",
-        link: "https://drive.google.com/file/d/1aJIwZFPAAZPnNjUOAVxIiMdKaMSp1AHG/preview",
+        link: "https://drive.google.com/file/d/1aJIwZFPAAZPnNjUOAVxIiMdKaMSp1AHG/view",
       },
       {
         name: "Python unit 2",
-        link: "https://drive.google.com/file/d/11rmY8Dq9uZiTJu4bpMwd4Gs6wTG_lk9_/preview",
+        link: "https://drive.google.com/file/d/11rmY8Dq9uZiTJu4bpMwd4Gs6wTG_lk9_/view",
       },
       {
         name: "Python unit 3",
-        link: "https://drive.google.com/file/d/1baE0ofG7z567YlbIydm3nD2vxBRBS26N/preview",
+        link: "https://drive.google.com/file/d/1baE0ofG7z567YlbIydm3nD2vxBRBS26N/view",
       },
       {
         name: "Python unit 4",
-        link: "https://drive.google.com/file/d/1a0TkB_S2BPgn9dorCTVKwaA64yOZFZdR/preview",
+        link: "https://drive.google.com/file/d/1a0TkB_S2BPgn9dorCTVKwaA64yOZFZdR/view",
       },
       {
         name: "Python unit 5",
-        link: "https://drive.google.com/file/d/1Q-BPGeXuFnW5f1HQOdmNtzk7Jww1m1I4/preview",
+        link: "https://drive.google.com/file/d/1Q-BPGeXuFnW5f1HQOdmNtzk7Jww1m1I4/view",
       },
     ],
   },
@@ -345,7 +345,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "TAFL Unit-01 (1)",
-            link: "https://drive.google.com/file/d/10MhzYjAOwNvG7bNPTqog7huPJJUChuh7/preview",
+            link: "https://drive.google.com/file/d/10MhzYjAOwNvG7bNPTqog7huPJJUChuh7/view",
           },
         ],
       },
@@ -354,7 +354,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "REGULAR EXPRESSION & LANGUAGE",
-            link: "https://drive.google.com/file/d/1v0WmCoBpAOTHVYkv2sr9nQqpI5coZkrV/preview",
+            link: "https://drive.google.com/file/d/1v0WmCoBpAOTHVYkv2sr9nQqpI5coZkrV/view",
           },
         ],
       },
@@ -363,7 +363,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "REGULAR AND NON-REGULAR GRAMMERS",
-            link: "https://drive.google.com/file/d/1Y1d1dZQb3QzTzRI6ubKJ_PDDEsUK0gig/preview",
+            link: "https://drive.google.com/file/d/1Y1d1dZQb3QzTzRI6ubKJ_PDDEsUK0gig/view",
           },
         ],
       },
@@ -372,7 +372,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "UNIT 4 (2)",
-            link: "https://drive.google.com/file/d/1C6gRHl7Cf9yHngz5Yn0ZbCryis0McQnf/preview",
+            link: "https://drive.google.com/file/d/1C6gRHl7Cf9yHngz5Yn0ZbCryis0McQnf/view",
           },
         ],
       },
@@ -382,7 +382,7 @@ let folders: Folder[] = [
         folders: [
           {
             name: "Theory of Automata and Formal Languages_3",
-            link: "https://drive.google.com/file/d/1p86-gVAWYPxqIabqCmBrolDqxXJZ999o/preview",
+            link: "https://drive.google.com/file/d/1p86-gVAWYPxqIabqCmBrolDqxXJZ999o/view",
           },
         ],
       },
@@ -393,27 +393,27 @@ let folders: Folder[] = [
     folders: [
       {
         name: "COPY of UHV notes .pdf",
-        link: "https://drive.google.com/file/d/1sETLYC0X3b4hg4-CFa-hAI4ofcVGroPW/preview",
+        link: "https://drive.google.com/file/d/1sETLYC0X3b4hg4-CFa-hAI4ofcVGroPW/view",
       },
       {
         name: "Unit 1 .pdf",
-        link: "https://drive.google.com/file/d/1BLHL7wQx8ZQC3Wwt3H3Xjau5lfKZ62Fe/preview",
+        link: "https://drive.google.com/file/d/1BLHL7wQx8ZQC3Wwt3H3Xjau5lfKZ62Fe/view",
       },
       {
         name: "Unit 2 .pdf",
-        link: "https://drive.google.com/file/d/1Po5wuQ3w1MVyGmv4YW7lkLKOYMtth5fK/preview",
+        link: "https://drive.google.com/file/d/1Po5wuQ3w1MVyGmv4YW7lkLKOYMtth5fK/view",
       },
       {
         name: "Unit 3 .pdf",
-        link: "https://drive.google.com/file/d/17__YyXxNupV2D071Sa41ZAHGy1Ku7pE-/preview",
+        link: "https://drive.google.com/file/d/17__YyXxNupV2D071Sa41ZAHGy1Ku7pE-/view",
       },
       {
         name: "Unit 4 .pdf",
-        link: "https://drive.google.com/file/d/14SvxMCJKnGc2AXl-2kHZhKaqvXYEuqzb/preview",
+        link: "https://drive.google.com/file/d/14SvxMCJKnGc2AXl-2kHZhKaqvXYEuqzb/view",
       },
       {
         name: "Unit 5 .pdf",
-        link: "https://drive.google.com/file/d/11rlcSQwq398_LrQ_u24VkS6bWmfP94Bf/preview",
+        link: "https://drive.google.com/file/d/11rlcSQwq398_LrQ_u24VkS6bWmfP94Bf/view",
       },
     ],
   },
@@ -422,27 +422,27 @@ let folders: Folder[] = [
     folders: [
       {
         name: "Unit 1 .pdf",
-        link: "https://drive.google.com/file/d/1KeUWE7djzaaz2_hn0Rc0DQAUSMLYeJaE/preview",
+        link: "https://drive.google.com/file/d/1KeUWE7djzaaz2_hn0Rc0DQAUSMLYeJaE/view",
       },
       {
         name: "Unit 2 .pdf",
-        link: "https://drive.google.com/file/d/1Z5Zfj89TI9cZXIILzqkT6AJTgw8gqLEk/preview",
+        link: "https://drive.google.com/file/d/1Z5Zfj89TI9cZXIILzqkT6AJTgw8gqLEk/view",
       },
       {
         name: "Unit 3 .pdf",
-        link: "https://drive.google.com/file/d/1_vINi9cvgBndCiVxZz8p4sWLPaY9LAwr/preview",
+        link: "https://drive.google.com/file/d/1_vINi9cvgBndCiVxZz8p4sWLPaY9LAwr/view",
       },
       {
         name: "Unit 4 .pdf",
-        link: "https://drive.google.com/file/d/1_vINi9cvgBndCiVxZz8p4sWLPaY9LAwr/preview",
+        link: "https://drive.google.com/file/d/1_vINi9cvgBndCiVxZz8p4sWLPaY9LAwr/view",
       },
       {
         name: "Unit 5 .pdf",
-        link: "https://drive.google.com/file/d/19q_YRsiX11etybQvYMa3nbEhy9GmVfuG/preview",
+        link: "https://drive.google.com/file/d/19q_YRsiX11etybQvYMa3nbEhy9GmVfuG/view",
       },
       {
         name: "Web Designing .pdf",
-        link: "https://drive.google.com/file/d/1bycMykDZSogVNqLA4kOG_f4gJxylBSRB/preview",
+        link: "https://drive.google.com/file/d/1bycMykDZSogVNqLA4kOG_f4gJxylBSRB/view",
       },
     ],
   },
