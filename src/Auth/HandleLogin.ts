9d807d0fc048f91f8ddb53bcad0be13feae31714
krishkh <krishkhanna00@gmail.com>
2024-11-13 01:16:02 +0530

    added logout button funcionality, and added logo, also removed unnecesary stuff (commented it and a convenience of code change for the way we are calling apis)

--- src/Auth/HandleLogin.ts
+++ src/Auth/HandleLogin.ts
@@ -6,10 +6,10 @@ interface Props {
 }
 
 export const HandleLogin = async (props: Props) => {
-  const apiKey = import.meta.env.VITE_BASE_URL;
+  const apiKey: string = import.meta.env.VITE_BASE_URL;
   try {
     console.log(props);
-    const response = await fetch(apiKey.concat("/login"), {
+    const response = await fetch(`${apiKey}/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/Auth/HandleRegister.ts
+++ src/Auth/HandleRegister.ts
@@ -10,7 +10,7 @@ const apiKey = import.meta.env.VITE_BASE_URL;
 
 const HandleRegister = async (props: Props) => {
   try {
-    const response = await fetch(apiKey.concat("/register"), {
+    const response = await fetch(`${apiKey}/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
