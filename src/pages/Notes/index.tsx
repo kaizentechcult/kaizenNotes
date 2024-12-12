28a9fc473f5f4271e1d69cfea93a2f22cf51e3a5
sooraj002 <soorajnambiar8@gmail.com>
2024-12-12 21:29:40 +0530

    idk

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -1,27 +1,40 @@
-import { Component, createSignal, onMount } from "solid-js";
+import { Component, createSignal, For, onMount } from "solid-js";
+
+type File = {
+  name: string;
+  size: number;
+  path: string;
+};
 
 const Notes: Component = () => {
   const apiKey = import.meta.env.VITE_BASE_URL;
-  const [data, setData] = createSignal<any[]>([]);
+  const [data, setData] = createSignal<File[]>([]);
 
   onMount(async () => {
-    const res = await fetch(`${apiKey}/data/files`);
-    const files = await res.json();
-    console.log(files);
-    setData(files);
+    try {
+      const res = await fetch(`${apiKey}/mega/files`);
+      if (!res.ok) {
+        throw new Error(`Failed to fetch data: ${res.statusText}`);
+      }
+      const files = await res.json();
+      setData(files);
+      console.log(data());
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   });
 
   return (
     <div>
       <h1>Hello World</h1>
       <p>This is a sample SolidJS page.</p>
-      {data().length > 0 && (
-        <ul>
-          {data().map((file) => (
-            <li>{file}</li>
-          ))}
-        </ul>
-      )}
+      <ul>
+        <li>
+          <p>Name: {data()[0]?.name}</p>
+          <p>Size: {data()[0]?.size}</p>
+          <p>Path: {data()[0]?.path}</p>
+        </li>
+      </ul>
     </div>
   );
 };
